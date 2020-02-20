f9c0df66e7a610e9bfdf6a92ca0a83cf45e4ee8d
Jeremy Hart <jerome215@comcast.net>
2020-02-20 16:08:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def roll_call_dwarves(array)# code your agruement here
   i = 0
   while i < array.length
@@ -16,11 +17,33 @@ end
 def long_planeteer_calls(long_planeteer_calls)
   answer = false
   long_planeteer_calls.each do |call|
+=======
+def roll_call_dwarves(dwarfs)# code an argument here
+    dwarfs.each_with_index do | name , index |
+    puts "#{index + 1} #{name}"
+  end
+end
+
+def summon_captain_planet(veggies)# code an argument here
+    veggies.collect do |call| 
+    call.capitalize + "!"
+  end
+
+end
+
+def long_planteer_calls(long_planteer_calls)# code an argument here
+  answer = false
+  long_planteer_calls.each do |call|
+>>>>>>> ab4cd8133d950544fe363ac70548b77d2c568ae2
     if call.length > 4
       answer = true
     end
   end
+<<<<<<< HEAD
   answer
+=======
+    answer
+>>>>>>> ab4cd8133d950544fe363ac70548b77d2c568ae2
 end
 
 def find_the_cheese(cheese)# code an argument here
