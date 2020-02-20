ab0462638beb66267678aeca76c80659ca14f784
Jeremy Hart <jerome215@comcast.net>
2020-02-20 16:18:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)# code your agruement here
   while i < array.length
     puts "#{i + 1}. #{array[i]}"
     i += 1
-  end # Your code here
+  end
 end
 
 def summon_captain_planet(veggies)# code an argument here
